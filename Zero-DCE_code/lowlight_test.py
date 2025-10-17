ab5bc740e76f7f3ef881ee15fc27a3f873c7efea
nathan <nathancai1031@gmail.com>
2025-10-17 14:02:42 -0400

    initial commit

--- Zero-DCE_code/lowlight_test.py
+++ Zero-DCE_code/lowlight_test.py
@@ -18,45 +18,46 @@ import time
 
  
 def lowlight(image_path):
-	os.environ['CUDA_VISIBLE_DEVICES']='0'
-	data_lowlight = Image.open(image_path)
+    # Set device for Apple Silicon MPS or fallback to CPU
+    device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+    
+    data_lowlight = Image.open(image_path)
 
- 
+    data_lowlight = (np.asarray(data_lowlight)/255.0)
 
-	data_lowlight = (np.asarray(data_lowlight)/255.0)
+    data_lowlight = torch.from_numpy(data_lowlight).float()
+    data_lowlight = data_lowlight.permute(2,0,1)
+    data_lowlight = data_lowlight.to(device).unsqueeze(0)  # Changed .cuda() to .to(device)
 
+    DCE_net = model.enhance_net_nopool().to(device)  # Changed .cuda() to .to(device)
+    DCE_net.load_state_dict(torch.load('snapshots/Epoch99.pth', map_location=device))  # Added map_location
+    
+    start = time.time()
+    _,enhanced_image,_ = DCE_net(data_lowlight)
 
-	data_lowlight = torch.from_numpy(data_lowlight).float()
-	data_lowlight = data_lowlight.permute(2,0,1)
-	data_lowlight = data_lowlight.cuda().unsqueeze(0)
+    end_time = (time.time() - start)
+    print(end_time)
+    
+    # Move tensor back to CPU for saving
+    enhanced_image = enhanced_image.cpu()
+    
+    image_path = image_path.replace('test_data','result')
+    result_path = image_path
+    if not os.path.exists(image_path.replace('/'+image_path.split("/")[-1],'')):
+        os.makedirs(image_path.replace('/'+image_path.split("/")[-1],''))
 
-	DCE_net = model.enhance_net_nopool().cuda()
-	DCE_net.load_state_dict(torch.load('snapshots/Epoch99.pth'))
-	start = time.time()
-	_,enhanced_image,_ = DCE_net(data_lowlight)
-
-	end_time = (time.time() - start)
-	print(end_time)
-	image_path = image_path.replace('test_data','result')
-	result_path = image_path
-	if not os.path.exists(image_path.replace('/'+image_path.split("/")[-1],'')):
-		os.makedirs(image_path.replace('/'+image_path.split("/")[-1],''))
-
-	torchvision.utils.save_image(enhanced_image, result_path)
+    torchvision.utils.save_image(enhanced_image, result_path)
 
 if __name__ == '__main__':
 # test_images
-	with torch.no_grad():
-		filePath = 'data/test_data/'
-	
-		file_list = os.listdir(filePath)
+    with torch.no_grad():
+        filePath = 'data/test_data/'
+    
+        file_list = os.listdir(filePath)
 
-		for file_name in file_list:
-			test_list = glob.glob(filePath+file_name+"/*") 
-			for image in test_list:
-				# image = image
-				print(image)
-				lowlight(image)
-
-		
-
+        for file_name in file_list:
+            test_list = glob.glob(filePath+file_name+"/*") 
+            for image in test_list:
+                # image = image
+                print(image)
+                lowlight(image)
