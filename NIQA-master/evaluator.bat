392b9d5201401966b3a47403aa07c073c58b34b6
AnissHamouda <arhamouda04@gmail.com>
2025-11-30 17:15:56 -0500

    Update evaluator.bat

--- NIQA-master/evaluator.bat
+++ NIQA-master/evaluator.bat
@@ -1,6 +1,6 @@
-python main.py --folder ./images/demo1 ^
+python main.py --folder /content/Zero-DCE/Zero-DCE_code/data/result/DICM ^
 --metric BRISQUE PIQE NIQE ^
 --ext jpg png ^
---record ./images/demo1.csv ^
+--record /content/Zero-DCE/Zero-DCE_code/data/result/DICM/result_eval.csv ^
 --gpu True ^
 --each_record t
