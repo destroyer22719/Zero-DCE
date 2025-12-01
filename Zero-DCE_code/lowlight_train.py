e9cb7291b41f7b302b9f431e7aac226b0342c31b
m1tchelln <mitchell.nagler@gmail.com>
2025-11-30 20:29:48 -0500

    changed epochs back

--- Zero-DCE_code/lowlight_train.py
+++ Zero-DCE_code/lowlight_train.py
@@ -94,7 +94,7 @@ if __name__ == "__main__":
 	parser.add_argument('--lr', type=float, default=0.00001)
 	parser.add_argument('--weight_decay', type=float, default=0.0001)
 	parser.add_argument('--grad_clip_norm', type=float, default=0.05)
-	parser.add_argument('--num_epochs', type=int, default=50)
+	parser.add_argument('--num_epochs', type=int, default=200)
 	parser.add_argument('--train_batch_size', type=int, default=8)
 	parser.add_argument('--val_batch_size', type=int, default=4)
 	parser.add_argument('--num_workers', type=int, default=4)
